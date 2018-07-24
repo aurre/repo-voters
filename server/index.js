5acf02e8cd5bbc31d5f80f6b921f3a892e9639f4
aurre <aurrecochearaysa@gmail.com>
2018-07-23 23:24:54 -0500

    lates chages

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ const createApp = () => {
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
 
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    });
+
     // compression middleware
     app.use(compression())
 
